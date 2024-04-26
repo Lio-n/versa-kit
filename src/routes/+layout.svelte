f07b1361cd224ad514b0f3b09ac5a64f42913dec
Lio-n <leonardofontan14@gmail.com>
2024-04-26 18:52:19 -0300

    feat(TailwindCSS):set config

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+<script>
+	import '../app.css';
+</script>
+
+<svelte:head>
+	<title>VersaKit</title>
+</svelte:head>
+
+<div>
+	<main>
+		<slot />
+	</main>
+</div>
+
+<style>
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,5 @@
 <script>
-	import Main from '../pages/main/Main.svelte';
 </script>
 
-<h1>Welcome to SvelteKit</h1>
+<h1 class="text-3xl font-bold underline text-blue-700">Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<Main />
