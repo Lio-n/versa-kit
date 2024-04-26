aef3fb9ea2ee7a3a5d8d37f32bb71aa89a27c217
Lio-n <leonardofontan14@gmail.com>
2024-04-26 18:28:40 -0300

    feat:basic structure

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
+<script>
+	import Main from '../pages/main/Main.svelte';
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<Main />
